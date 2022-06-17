dfec47e31fcc4302e5c9af3c99f3b08c0e346e27
TheUnforg1ven <tbatjuk4u@gmail.com>
2022-06-17 18:16:49 +0300

    Added selection sort

--- SelectionSort03/Program.cs
+++ SelectionSort03/Program.cs
@@ -1,3 +1,27 @@
-﻿
+﻿int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+Console.WriteLine(string.Join(" | ", integerValues));
+SelectionSort(integerValues);
+Console.WriteLine(string.Join(" | ", integerValues));
 
-Console.WriteLine("Hello, World!");
+static void SelectionSort(IList<int> arr)
+{
+    for (var i = 0; i < arr.Count - 1; i++)
+    {
+        var minValue = arr[i];
+        var minIndex = i;
+
+        for (var j = i + 1; j < arr.Count; j++)
+        {
+            if (arr[j] < minValue)
+            {
+                minValue = arr[j];
+                minIndex = j;
+            }
+        }
+        
+        Swap(arr, i, minIndex);
+    }
+}
+
+static void Swap(IList<int> arr, int first, int second)
+    => (arr[first], arr[second]) = (arr[second], arr[first]);
