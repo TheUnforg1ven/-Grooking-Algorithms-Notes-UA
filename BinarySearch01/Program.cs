c79db6054cc13144f5c53211ff52f64df8271773
TheUnforg1ven <tbatjuk4u@gmail.com>
2022-06-17 16:44:22 +0300

    Fixed all projects to .net6

--- BinarySearch01/Program.cs
+++ BinarySearch01/Program.cs
@@ -1,47 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿var myArr = new List<int>();
+for (var i = 0; i < 20; i++)
+    myArr.Add(i);
 
-namespace BinarySearch01
+myArr.Sort();
+
+var founded = BinarySearch(myArr, 12);
+Console.WriteLine($"Found value is: {founded}");
+
+
+static int BinarySearch(IList<int> arr, int value)
 {
-    internal class Program
+    if (arr.Count == 0)
+        return default;
+
+    var first = 0;
+    var last = arr.Count - 1;
+
+    while (first < last)
     {
-        static void Main(string[] args)
+        var middle = (first + last) / 2;
+
+        if (value < arr[middle]) 
+            last = middle - 1;
+        else if(value > arr[middle])
+            first = middle + 1;
+        else
         {
-            var myArr = new List<int>();
-            for (var i = 0; i < 20; i++)
-                myArr.Add(i);
-
-            myArr.Sort();
-
-            var founded = BinarySearch(myArr, 12);
-            Console.WriteLine($"Found value is: {founded}");
-        }
-
-        private static int BinarySearch(IList<int> arr, int value)
-        {
-            if (arr.Count == 0)
-                return default;
-
-            var first = 0;
-            var last = arr.Count - 1;
-
-            while (first < last)
-            {
-                var middle = (first + last) / 2;
-
-                if (value < arr[middle]) 
-                    last = middle - 1;
-                else if(value > arr[middle])
-                    first = middle + 1;
-                else
-                {
-                    value = arr[middle];
-                    return value;
-                }
-            }
-
-            Console.WriteLine($"There are no needed value in current array");
-            return default;
+            value = arr[middle];
+            return value;
         }
     }
+
+    Console.WriteLine("There are no needed value in current array");
+    return default;
 }
