f77c0caef787c0db5cde68b651e4852fcdf384d8
TheUnforg1ven <tbatjuk4u@gmail.com>
2022-07-28 16:56:34 +0300

    Added quicksort

---
+++ Quicksort07/Program.cs
@@ -0,0 +1,42 @@
+﻿int[] integerValues = { 5, 8, 1, 3, 7, 9, 2, 4};
+Console.WriteLine(string.Join(" | ", integerValues));
+Sort(integerValues);
+Console.WriteLine(string.Join(" | ", integerValues));
+
+static void Sort(IList<int> arr)
+{
+    QuickSort(arr, 0, arr.Count - 1);
+}
+
+static void QuickSort(IList<int> arr, int lower, int upper)
+{
+    if (lower < upper)
+    {
+        var part = Partition(arr, lower, upper);
+        QuickSort(arr, lower, part);
+        QuickSort(arr, part + 1, upper);
+    }
+}
+
+static int Partition(IList<int> array, int lower, int upper)
+{
+    var pivot = array[lower];
+
+    // all elements lower than pivot on the left, all elements greater than pivot on the right
+    do
+    {
+        while (array[lower] < pivot) // 1) find 'array[lower] > pivot'
+            lower++;
+        while (array[upper] > pivot) // 2) find 'array[upper] < pivot'
+            upper--;
+        if (lower >= upper)  
+            break; 
+        Swap(array, lower, upper); // 3) Swap them, do it while 'lower < upper'
+    }
+    while (lower < upper);
+    
+    return upper;
+}
+
+static void Swap(IList<int> arr, int first, int second)
+    => (arr[first], arr[second]) = (arr[second], arr[first]);
